984cbafab091432a4cfaa0b9a771a81fce6195aa
Janet <vongjanet@yahoo.com>
2018-05-14 07:00:41 -0700

    added docstrings

--- media.py
+++ media.py
@@ -2,17 +2,18 @@ import webbrowser
 
 
 class Movie():
-    """ This is the documentation. class variables, outside of init function,
-    google likes to keep contants as all caps. """
+    """ This is a class for defining the blueprint of Movie objects. """
 
     valid_ratings = ["G", "PG", "PG-13", "R"]
 
     # intializes title, storyline, poster image, and trailer.
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """Initializes class and defines variables with incoming parameter values"""
         self.title = movie_title
         self.storyline = movie_storyline,
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Test to see if the movie trailer opens in webbrowser."""
         webbrowser.open(self.trailer_youtube_url)  # Open trailer in webbrowser
